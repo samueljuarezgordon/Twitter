e5841107a83b30fc1c39d763fc21acc857382391
Samuel Juárez Gordon <9005596@alumnos.ufv.es>
2023-01-20 11:46:39 +0100

    Creados los proyectos twitterVaadin y twitterApi

---
+++ twitterApi/src/main/java/dis/ufv/twitterApi/twitterApi/TwitterApiApplication.java
@@ -0,0 +1,13 @@
+package dis.ufv.twitterApi.twitterApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApiApplication.class, args);
+	}
+
+}
---
+++ twitterApi/src/test/java/dis/ufv/twitterApi/twitterApi/TwitterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dis.ufv.twitterApi.twitterApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
